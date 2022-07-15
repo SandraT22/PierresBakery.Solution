bbabc5a29e8b236044feffa321d7008c20ddae9f
Sandra Tena <sandratena06@gmail.com>
2022-07-15 11:59:02 -0700

    Add code for pastry test 3

--- PierresBakery/Models/BakeryPastry.cs
+++ PierresBakery/Models/BakeryPastry.cs
@@ -7,13 +7,19 @@ namespace PierresBakery
     public static int PastryPrice(int pastryNum)
     {
       int pastryPrice = 0;
+      decimal pNum = Math.Floor((decimal) (pastryNum/3));
+      int pDecDealPrice = Decimal.ToInt32(pNum) *10;
       int pDealPrice = (pastryNum/3) * 5; //(use method to round down)
+      int pAddPrice = 0; (breadNum % 3) * 2
       if (pastryNum <= 2) {
       pastryPrice = pastryNum * 2;
         return pastryPrice;
       } else if (pastryNum % 3 == 0) {
       pastryPrice = pDealPrice;
-        return 0;
+        return pastryPrice;
+      } else if (pastryNum % 3 > 0) {
+      pastryPrice = pDecDealPrice + pAddPrice; 
+        return pastryPrice;
       } else {
         return pastryPrice;
       }
